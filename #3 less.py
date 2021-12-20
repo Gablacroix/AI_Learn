49ff5ea77bde4b5b9dba6a0157f16d630c97c289
SteelShark <Gablacroix7@gmail.com>
2021-12-20 16:10:56 +0300

    Поправлен синтаксис согласно PEP8

--- #3 less.py
+++ #3 less.py
@@ -8,4 +8,6 @@ cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2),
 cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 50, (120, 252, 50), thickness=1)
 cv2.putText(photo, 'SteelShark', (100, 150), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), thickness=1)
 cv2.imshow('Photo', photo)
+# print(cv2.__version__)
+
 cv2.waitKey(0)
--- #4 less.py
+++ #4 less.py
@@ -3,19 +3,30 @@ import numpy as np
 
 img = cv2.imread('images/icon.png')
 new_img = np.zeros(img.shape, dtype='uint8')
-#img = cv2.flip(img, 1)
+
+
+# img = cv2.flip(img, 1)
+
+
 def rotate(img_param, angle):
     height, width = img_param.shape[:2]
     point = (width // 2, height // 2)
-
     mat = cv2.getRotationMatrix2D(point, angle, 1)
     return cv2.warpAffine(img_param, mat, (width, height))
-#img = rotate(img, 90)
-def tranform(img_param, x, y):
+
+
+#  img = rotate(img, 90)
+
+
+def transform(img_param, x, y):
     mat = np.float32([[1, 0, x], [0, 1, y]])
-    return cv2.warpAffine(img_param,mat, (img_param.shape[1], img_param.shape[0]))
-#img = tranform(img, 30, 30 )
-#img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+    return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+
+
+# img = transform(img, 30, 30 )
+# img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.GaussianBlur(img, (5, 5), 0)
 img = cv2.Canny(img, 20, 20)
